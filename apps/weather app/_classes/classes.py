8d82bbe45d7fbd8508497a1fbace230758b89b21
Ethan Humphreys <116691290+K-972@users.noreply.github.com>
2024-05-27 14:12:25 +0100

    h

--- apps/weather app/_classes/classes.py
+++ apps/weather app/_classes/classes.py
@@ -1,18 +1,64 @@
 import requests
+import os
+import sys
 
 
-
-
-class weather:
+class weather():
     def __init__(self) -> None:
         self.apikey = None
         self.temperature = None
         self.pressure = None
         self.humidity = None
-        self.descriptio = None
+        self.description = None
 
     def __str__(self) -> str:
         return 'This class grabs the weathera and is not the gui of the main loop'
     
-    def ReadApiKey(self): # does it pull from the directory of the class
-        with open()
+    def ReadApiKey(self): # does it pull from the directory of the class file or the main file?
+        with open("apikey.txt", 'r', encoding='utf-8') as f:
+            self.apikey = f.read()
+            
+    def GetWeather(self):
+        
+        city_name = "Nuneaton"
+
+        base_url = "http://api.openweathermap.org/data/2.5/weather?"
+
+        complete_url = base_url + "appid=" + self.apikey + "&q=" + city_name
+
+        response = requests.get(complete_url)
+
+        x = response.json()
+
+        if x["cod"] != "404":
+
+            y = x["main"]
+
+            self.temperature = y["temp"]
+
+            self.pressure = y["pressure"]
+
+            self.humidity = y["humidity"]
+
+            z = x["weather"]
+            
+            self.description = z[0]["description"]
+
+            print(" Temperature (in celsius unit) = " +
+                str(self.temperature - 273.15) +
+                "\n Atmospheric pressure (in hPa unit) = " +
+                str(self.pressure) +
+                "\n Humidity (in percentage) = " +
+                str(self.humidity) +
+                "\n Description = " +
+                str(self.description))
+        else:
+            print(" City Not Found ")
+            
+            
+            
+            
+
+
+            
+    
---
+++ apps/weather app/main.py
@@ -0,0 +1,52 @@
+import sys
+import os
+import eel
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '_classes')) # go into calssed dir
+
+from classes import weather # imports the calsses
+
+
+
+
+# reset dir to script location
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+
+
+
+
+
+
+
+
+eel.init('_web') # initialize    
+
+@eel.expose 
+def HelloWorld():
+    print('Hello World!')
+
+eel.start('index.html', size=(800, 600))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Updates the weather stats
+def GrabWeather():
+    object = weather()
+    object.ReadApiKey()
+    object.GetWeather()
+    
+
+
