c233d77fdfd6dc46642d11e96908f39a29b1cd2c
K-972 <116691290+K-972@users.noreply.github.com>
2024-10-02 11:26:24 +0100

    tg

--- apps/darts/_classes/camera.py
+++ apps/darts/_classes/camera.py
@@ -1,5 +1,60 @@
+import shutil
 import cv2
 import time
+import os
+import shutil
+import random
+
+
+
+
+class CameraController:
+
+    def __init__(self) -> None:
+        self.filepath = '/home/toor/Documents/GitHub/Programming-Project/apps/darts/images'
+
+
+    def take_image(self):
+        cap = cv2.VideoCapture(0)
+
+        if not cap.isOpened():
+            print("Error: Could not open camera.")
+            return
+
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+        ret, frame = cap.read()
+        if ret:
+            image_filename = f"{self.filepath}_{random.randint(0, 1000)}.jpg"
+            cv2.imwrite(image_filename, frame)
+            print(f"Image saved as: {image_filename}")
+        else:
+            print("Error: Could not capture image.")
+
+        cap.release()
+
+    def wipe_image_folder(self):
+        for filename in os.listdir(self.filepath):
+            file_path = os.path.join(self.filepath, filename)
+            try:
+                if os.path.isfile(file_path) or os.path.islink(file_path):
+                    os.unlink(file_path)
+                elif os.path.isdir(file_path):
+                    shutil.rmtree(file_path)
+            except Exception as e:
+                print(f"Failed to delete {file_path}. Reason: {e}")
+
+
+        
+
+
+
+
+
+
+# test function to take images and calculate average time taken
+# just for testing
 
 def take_images_and_calculate_average(filename_base, interval, num_images):
     cap = cv2.VideoCapture(0)
@@ -37,4 +92,11 @@ def take_images_and_calculate_average(filename_base, interval, num_images):
     print(f"Average time taken for each image (including wait): {average_time:.4f} seconds")
 
 
-take_images_and_calculate_average(filename_base='output_image', interval=0.5, num_images=100)
+#take_images_and_calculate_average(filename_base='output_image', interval=0.5, num_images=100)
+
+
+main = CameraController()
+
+while True:
+    
+    main.take_image()
