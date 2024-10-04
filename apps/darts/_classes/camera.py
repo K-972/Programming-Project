cb891c2976a2262bcb78f669b593ced6e5ad0678
K-972 <116691290+K-972@users.noreply.github.com>
2024-10-04 09:59:10 +0100

    g

--- apps/darts/_classes/camera.py
+++ apps/darts/_classes/camera.py
@@ -46,50 +46,35 @@ class CameraController:
                 print(f"Failed to delete {file_path}. Reason: {e}")
 
 
-        
+    def take_images_constantly(self, interval):
+        cap = cv2.VideoCapture(0)
+
+        if not cap.isOpened():
+            print("Error: Could not open camera.")
+            return
+
+        cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+        cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+        i = 0
+        while True:
+            i += 1
+            
+            ret, frame = cap.read()
+            if ret:
+                image_filename = f"{self.filepath}_{i+1}.jpg"
+                cv2.imwrite(image_filename, frame)
+                print(f"Image {i+1} saved as: {image_filename}")
+            else:
+                print("Error: Could not capture image.")
+                break
+            
+
+            time.sleep(interval)
 
 
+        cap.release()
 
-
-
-
-# test function to take images and calculate average time taken
-# just for testing
-
-def take_images__constantly(interval):
-    cap = cv2.VideoCapture(0)
-
-    if not cap.isOpened():
-        print("Error: Could not open camera.")
-        return
-
-    cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
-    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
-
-    total_time = 0
-
-    for i in range(num_images):
-        start_time = time.time()
-        
-        ret, frame = cap.read()
-        if ret:
-            image_filename = f"{filename_base}_{i+1}.jpg"
-            cv2.imwrite(image_filename, frame)
-            print(f"Image {i+1} saved as: {image_filename}")
-        else:
-            print("Error: Could not capture image.")
-            break
-        
-        elapsed_time = time.time() - start_time
-        total_time += elapsed_time
-        
-        time.sleep(interval)
-        total_time += interval
-
-    cap.release()
-
-    average_time = total_time / num_images if num_images > 0 else 0
-    print(f"Average time taken for each image (including wait): {average_time:.4f} seconds")
 
 
 #take_images_and_calculate_average(filename_base='output_image', interval=0.5, num_images=100)
@@ -97,6 +82,4 @@ def take_images__constantly(interval):
 
 main = CameraController()
 
-while True:
-    
-    main.take_image()
+main.take_images_constantly(0.5)
